8d0019f66144f03c88dec0ae3123b97d621e701f
adigars1 <143882179+adigars1@users.noreply.github.com>
2024-05-07 21:15:11 +0300

    код похожий на взлом
    просто для красоты

---
+++ vzlom.bat
@@ -0,0 +1,6 @@
+@echo off 
+color 0a
+title --------------VIA HACKER--------------
+:top
+echo %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random%  %random% 
+goto top
